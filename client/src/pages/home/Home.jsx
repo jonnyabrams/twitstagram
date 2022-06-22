d5d55833d3fae0989d8a03012c7d98bdb34deaef
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-22 09:41:22 +0100

    Set up skeleton Home page & brought it into App

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="profileSide">Profile</div>
+      <div className="postSide">Posts</div>
+      <div className="rightSide">Rightside</div>
+    </div>
+  )
+}
+
+export default Home
