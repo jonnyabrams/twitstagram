06c7b5b5c75460cdda5b70a3429dff195a1f3f3a
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-24 15:04:40 +0100

    Create login route and controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,3 +16,22 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await UserModel.findOne({ email: email })
+
+    if (user) {
+      const valid = await bcrypt.compare(password, user.password)
+
+      valid ? res.status(200).json(user) : res.status(400).json("Wrong password")
+    } else {
+      res.status(404).json("User does not exist")
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+
+}
