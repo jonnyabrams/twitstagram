4c32e1a4f855ba153289d4eb6b3017e18e4ca022
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-24 11:23:19 +0100

    Create User model with auth route/controller: * auth/register POST request works on ThunderClient

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import 'dotenv/config'
+import authRoute from './routes/authRoute.js'
 
 const app = express()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -11,3 +12,5 @@ mongoose
   .connect(process.env.MONGODB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(process.env.PORT, () => console.log(`Listening at port ${process.env.PORT}`)))
   .catch((error) => console.log(error))
+
+app.use('/auth', authRoute)
