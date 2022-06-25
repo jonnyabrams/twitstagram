cac00d4a12f80ee73b55e67812e5ced489be4a51
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-25 12:51:18 +0100

    JWT token added to signup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import UserModel from '../models/userModel.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const registerUser = async (req, res) => {
   const salt = await bcrypt.genSalt(10)
@@ -8,8 +9,12 @@ export const registerUser = async (req, res) => {
   const newUser = new UserModel(req.body)
 
   try {
-    await newUser.save()
-    res.status(200).json(newUser)
+    const user = await newUser.save()
+    const token = jwt.sign({
+      username: user.username,
+      id: user._id
+    }, process.env.JWT_KEY, { expiresIn: '1h' })
+    res.status(200).json({ user, token })
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
