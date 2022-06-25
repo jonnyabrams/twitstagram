8ae0fcf5b576f614c1f9d6ce6f08fd60937f14a8
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-25 10:37:49 +0100

    Check passwords match + clear form data

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -3,11 +3,41 @@ import { useState } from 'react'
 import Logo from '../../img/assets/twitstagram-logo.png'
 
 const Auth = () => {
-  const [isSignUp, setIsSignUp] = useState(false)
-  const [data, setData] = useState({ firstname: "", lastname: "", username: "", email: "", password: "", confirmpassword: "" })
+  const [isSignUp, setIsSignUp] = useState(true)
+  const [data, setData] = useState({ 
+    firstname: "", 
+    lastname: "", 
+    username: "", 
+    email: "", 
+    password: "", 
+    confirmpassword: "" 
+  })
+  const [passwordsMatch, setPasswordsMatch] = useState(true)
 
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(isSignUp) {
+      if (data.password !== data.confirmpassword) {
+        setPasswordsMatch(false)
+      }
+    }
+  }
+
+  const resetForm = () => {
+    setPasswordsMatch(true)
+    setData({
+      firstname: "", 
+      lastname: "", 
+      username: "", 
+      email: "", 
+      password: "", 
+      confirmpassword: "" 
+    })
   }
 
   return (
@@ -22,35 +52,37 @@ const Auth = () => {
       </div>
        {/* Right side */}
       <div className="auth_right">
-        <form className="info_form auth_form">
+        <form className="info_form auth_form" onSubmit={handleSubmit}>
           <h3>{ isSignUp ? "Sign up" : "Log in" }</h3>
 
           { isSignUp &&
             <>
               <div>
-                <input type="text" placeholder="First name" className="info_input" name="firstname" onChange={handleChange} />
-                <input type="text" placeholder="Last name" className="info_input" name="lastname" onChange={handleChange} />
+                <input type="text" placeholder="First name" className="info_input" name="firstname" onChange={handleChange} value={data.firstname} />
+                <input type="text" placeholder="Last name" className="info_input" name="lastname" onChange={handleChange} value={data.lastname} />
               </div> 
             
 
               <div>
-                <input type="text" placeholder="Username" className="info_input" name="username" onChange={handleChange} />
+                <input type="text" placeholder="Username" className="info_input" name="username" onChange={handleChange} value={data.username} />
               </div>
             </>
           }
 
           <div>
-            <input type="email" placeholder="Email" className="info_input" name="email" onChange={handleChange} />
+            <input type="email" placeholder="Email" className="info_input" name="email" onChange={handleChange} value={data.email} />
           </div>
 
           <div>
-            <input type="password" placeholder="Password" className="info_input" name="password" onChange={handleChange} />
-            { isSignUp && <input type="password" placeholder="Confirm password" className="info_input" name="confirmpassword" onChange={handleChange} /> }
+            <input type="password" placeholder="Password" className="info_input" name="password" onChange={handleChange} value={data.password} />
+            { isSignUp && <input type="password" placeholder="Confirm password" className="info_input" name="confirmpassword" onChange={handleChange} value={data.confirmpassword} /> }
           </div>
-
+          <span style={{ display: passwordsMatch ? "none" : "block", color: "red", fontSize: "12px", alignSelf: "flex-end", marginRight: "5px" }}>
+            * Passwords do not match
+          </span>
           <div>
-            <span style={{ fontSize: '12px', cursor: "pointer", color: "blue" }} onClick={ () => setIsSignUp(!isSignUp) }>
-              <u>{ isSignUp ? "Already have an account? Log in!" : "Don't have an account? Sign up!" }</u>
+            <span style={{ fontSize: '12px', cursor: "pointer", color: "blue", textDecoration: "underline" }} onClick={ () => { setIsSignUp(!isSignUp); resetForm() } }>
+              { isSignUp ? "Already have an account? Log in!" : "Don't have an account? Sign up!" }
             </span>
           </div>
           <button className="button info_button" type="submit">{ isSignUp ? "Sign up" : "Log in" }</button>
