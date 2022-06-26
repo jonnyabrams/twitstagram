619d326b2a0202f99354faea601b092b8273ead8
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-26 11:07:36 +0100

    Can like/unlike post

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -3,21 +3,32 @@ import Comment from '../../img/assets/comment.png'
 import Share from '../../img/assets/share.png'
 import Liked from '../../img/assets/like.png'
 import Unliked from '../../img/assets/unlike.png'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
+import { likePost } from '../../redux/api/postRequest'
 
 const Post = ({data}) => {
   const { user } = useSelector((state) => state.authReducer.authData)
+  const [liked, setLiked] = useState(data.likes.includes(user._id))
+  const [likes, setLikes] = useState(data.likes.length)
+
+  const handleLike = () => {
+    setLiked(!liked)
+    likePost(data._id, user._id)
+    liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1)
+  }
+
   return (
     <div className="post">
       <img src={data.image ? process.env.REACT_APP_PUBLIC_FOLDER + data.image : ""} alt="" />
 
       <div className="post_reactions">
-        <img src={data.liked ? Liked : Unliked} alt="" />
+        <img src={liked ? Liked : Unliked} alt="" style={{ cursor: "pointer" }} onClick={handleLike} />
         <img src={Comment} alt="" />
         <img src={Share} alt="" />
       </div>
 
-      <span style={{ color: "var(--gray)", fontSize: '12px' }}>{data.likes} likes</span>
+      <span style={{ color: "var(--gray)", fontSize: '12px' }}>{likes} likes</span>
 
       <div className="details">
         <span><b>{data.name}</b></span>
--- client/src/redux/api/postRequest.js
+++ client/src/redux/api/postRequest.js
@@ -3,3 +3,4 @@ import axios from 'axios'
 const API = axios.create({ baseURL: "http://localhost:8000" })
 
 export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`)
+export const likePost = (id, userId) => API.put(`post/${id}/like`, { userId: userId })
