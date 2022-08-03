7a582cea3bf8fd92dd042cc57cb45a10601aeac5
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-03 21:09:06 +0100

    feat: suggested users link to profiles

--- client/src/components/profileCard/ProfileCard.jsx
+++ client/src/components/profileCard/ProfileCard.jsx
@@ -2,33 +2,33 @@ import './profileCard.css'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-const ProfileCard = ({ location }) => {
-  const {user} = useSelector((state) => state.authReducer.authData)
+const ProfileCard = ({ location, user }) => {
+  const currentUser = useSelector((state) => state.authReducer.authData.user)
   const posts = useSelector((state) => state.postReducer.posts)
   const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER
 
   return (
     <div className="profile_card">
       <div className="profile_images">
-        <img src={user.coverPic ? serverPublic + user.coverPic : serverPublic + 'defaultCover.jpeg' } alt="" />
-        <img src={user.profilePic ? serverPublic + user.profilePic : serverPublic + 'defaultProfile.png' } alt="" />
+        <img src={user?.coverPic ? serverPublic + user?.coverPic : serverPublic + 'defaultCover.jpeg' } alt="" />
+        <img src={user?.profilePic ? serverPublic + user?.profilePic : serverPublic + 'defaultProfile.png' } alt="" />
       </div>
 
       <div className="profile_name">
-        <span>{user.firstname} {user.lastname}</span>
-        <span>{user.worksAt ? user.worksAt : "Write about yourself" }</span>
+        <span>{user?.firstname} {user?.lastname}</span>
+        <span>{user?.worksAt ? user?.worksAt : "Write about yourself" }</span>
       </div>
 
       <div className="follow_status">
         <hr />
         <div>
           <div className="follow">
-            <span>{user.following.length}</span>
+            <span>{user?.following?.length}</span>
             <span>Following</span>
           </div>
           <div className="vertical_line"></div>
           <div className="follow">
-            <span>{user.followers.length}</span>
+            <span>{user?.followers?.length}</span>
             <span>Followers</span>
           </div>
 
@@ -38,7 +38,7 @@ const ProfileCard = ({ location }) => {
 
               </div>
               <div className="follow">
-                <span>{ posts.filter((post) => post.userId === user._id).length }</span>
+                <span>{ posts.filter((post) => post.userId === user?._id).length }</span>
                 <span>posts</span>
               </div>
             </>
@@ -46,7 +46,7 @@ const ProfileCard = ({ location }) => {
         </div>
         <hr />
       </div>
-      {location === 'profilePage' ? '' : <span><Link style={{ textDecoration: "none", color: "inherit" }} to={ `/profile/${user._id}` }>My Profile</Link></span>}
+      {location === 'profilePage' ? '' : <span><Link style={{ textDecoration: "none", color: "inherit" }} to={ `/profile/${currentUser._id}` }>My Profile</Link></span>}
     </div>
   )
 }
